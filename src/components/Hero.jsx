e119dc69ec724cae7d05d40a1c39d1e1baa5d1d7
Erwin Alam <erwinalam4@gmail.com>
2023-04-25 11:53:48 +0700

    fix: hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,8 @@ export default function Hero({ data }) {
         }
     }, []);
     return (
-        <section className="relative">
-            <div className="hero mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+        <section className="hero">
+            <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
                 <div className="bg-[#605EEF] px-6 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-48 xl:col-span-6">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h1 className="w-1/2 mt-20 text-[68px] tracking-tight text-gray-900 sm:mt-10 sm:text-6xl break-words">
