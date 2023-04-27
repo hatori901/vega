8903194ba4f021e4b71d7e87888ea573366f5340
Erwin Alam <erwinalam4@gmail.com>
2023-04-27 12:21:29 +0700

    fix: text

--- src/components/liveOcean/Achievement.jsx
+++ src/components/liveOcean/Achievement.jsx
@@ -9,9 +9,10 @@ export default function Achievement() {
                 <h1 className='my-[96px] text-[68px] leading-[70px] text-black'>
                     Live Ocean racing is a <span className='text-[#605EEF]'>New Zealand racing</span> team competing at the highest level while championing action for the ocean.
                 </h1>
-                <p className='text-[25px] leading-[33px]'>
+                <p className='text-[25px] leading-[33px] mb-10'>
                     We champion action for the ocean by taking this message to the world through our sport.
-
+                </p>
+                <p className='text-[25px] leading-[33px]'>
                     Established by Peter Burling and Blair Tuke, Live Ocean Racing will bring together a talented and diverse team to compete at the highest level. With innovation and purpose baked into our DNA, we race on and for the ocean.
                 </p>
                 <div className='relative mt-[80px]'>
